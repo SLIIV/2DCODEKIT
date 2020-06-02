a41bca6f21f9f01948749cc3ea9c69932c1eef5e
SLIIV <YTANTEI@yandex.ru>
2020-06-02 13:45:11 +0900

    Added random spawnpoint

---
+++ Assets/RandomSpawnPoint/Scripts/RandomSpawnPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandomSpawnPoint : MonoBehaviour
+{
+    public int dotsCount;
+    public GameObject dotPrefab;
+    void Start()
+    {
+        for (int i = 0; i < dotsCount; i++)
+        {
+            Vector2 screenSize = new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
+            float randPointX = Random.Range(0, screenSize.x);
+            float randPointY = Random.Range(0, screenSize.y);
+            Vector2 randPoint = new Vector2(randPointX, randPointY);
+            randPoint = Camera.main.ScreenToWorldPoint(randPoint);
+            Instantiate(dotPrefab, randPoint, Quaternion.identity);
+        }
+    }
+
+}
