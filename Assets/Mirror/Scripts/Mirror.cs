e54eea9cddef57eb3b0074fd1afdd331ea6b0bf0
SLIIV <YTANTEI@yandex.ru>
2020-06-03 11:31:42 +0900

    Added Vector reflection

---
+++ Assets/Mirror/Scripts/Mirror.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    public Vector3 direction;
+    public Vector3 resultDirection;
+    public Vector3 normal;
+    public float speed;
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        resultDirection = Vector3.Reflect(direction, collision.transform.right);
+        direction = Vector3.Normalize(resultDirection);
+    }
+}
